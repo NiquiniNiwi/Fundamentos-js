bef5a6b29c45c958529040a6d608a6da2fd95133
Niquini <Niquini71@gmail.com>
2021-12-08 14:12:48 -0300

    Exemplos basicos do console

---
+++ console.js
@@ -0,0 +1,15 @@
+// Consol.api
+//.log
+
+const Variavel = true;
+console.log(245)
+
+console.log("Textin")
+
+console.log(Variavel)
+
+console.log("--------------------------")
+
+// Tratemento de erro!
+
+console.error("Deu erro!")
